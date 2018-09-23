1301129f385fa55f614f3adf5743bd647d20b034
Yuji Sakurai <yuji_sakurai@r.recruit.co.jp>
2018-09-23 16:11:57 +0900

    fix

--- main.py
+++ main.py
@@ -85,11 +85,6 @@ def review_cycle(resource_dict, review_dict):
 
 if __name__ == '__main__':
     file_path_list = glob.glob("./terraform/**.tf", recursive=True)
-    print(file_path_list)
-    for file_path in file_path_list:
-        if not re.match(".*.tf\Z", file_path):
-            file_path_list.remove(file_path)
-    print(file_path_list)
     for file_path in file_path_list:
         with codecs.open(file_path, 'r', 'utf-8') as fp:
             obj = hcl.load(fp)
