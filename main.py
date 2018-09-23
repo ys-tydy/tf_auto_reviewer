3d395ef72aace64084701a6051438abcabd53d26
Yuji Sakurai <yuji_sakurai@r.recruit.co.jp>
2018-09-23 16:03:23 +0900

    fix

--- main.py
+++ main.py
@@ -85,6 +85,7 @@ def review_cycle(resource_dict, review_dict):
 
 if __name__ == '__main__':
     file_path_list = glob.glob("./terraform/*", recursive=True)
+    print(file_path_list)
     for file_path in file_path_list:
         if not re.match(".*.tf\Z", file_path):
             file_path_list.remove(file_path)
